c03d09bb39afd6eb1088fd6850c5124ae8ac611d
PChome <zalupa@gmail.com>
2025-06-18 20:07:15 +0400

    ts

---
+++ lernTypeScript/compile.js
@@ -0,0 +1,14 @@
+"use strict";
+let myNumber = 42;
+let Str = "age: ";
+console.log(Str + myNumber);
+const nums = [1, 2, 3, 4];
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 2) {
+        console.log("тут етсь двойка");
+    }
+}
+const func = (num) => {
+    console.log(num);
+};
+func(12);
---
+++ lernTypeScript/compile.ts
@@ -0,0 +1,15 @@
+let myNumber: number = 42;
+let Str: string = "age: "
+console.log(Str + myNumber);
+
+const nums: Array<number> = [1,2,3,4]
+for(let i=0; i<nums.length; i++){
+    if(nums[i] === 2){
+        console.log("тут етсь двойка")
+    }
+}
+
+const func = (num: number): void => {
+    console.log(num);
+};
+func(12)
